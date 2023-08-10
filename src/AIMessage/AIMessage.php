e95fe3ae582d7050f4c620e0b57e582c52b035ca
Tim Anthony Alexander <info@timanthonyalexander.de>
2023-08-10 13:24:21 +0200

    Create most of chatgpt logic

---
+++ src/AIMessage/AIMessage.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TimAlexander\Aisearch\AIMessage;
+
+use TimAlexander\Aisearch\SystemConfig\SystemConfig;
+
+class AIMessage
+{
+    public string $role;
+    public string $content;
+}
--- src/ChatGPT/ChatGPT.php
+++ src/ChatGPT/ChatGPT.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace TimAlexander\Aisearch\ChatGPT;
 
+use JsonException;
+use TimAlexander\Aisearch\AIMessage\AIMessage;
 use TimAlexander\Aisearch\SystemConfig\SystemConfig;
 
 class ChatGPT
@@ -11,17 +13,84 @@ class ChatGPT
     public string $model = 'gpt-3.5-turbo-0301';
     public string $url = 'https://api.openapi.com/v1/chat/completions';
 
-    private string $chatPersistenceId;
+    private array $messages = [];
 
     public function __construct(
-        private SystemConfig $systemConfig
+        private SystemConfig $systemConfig,
+        private string $chatPersistenceId = null
     ) {
-        $this->chatPersistenceId = uniqid("", true);
+        $this->chatPersistenceId = $chatPersistenceId ?? uniqid("", true);
+    }
+
+    private function loadMessages(): void
+    {
+        $file = __DIR__ . '/../../config/chat/' . $this->chatPersistenceId . '.json';
+
+        if (!file_exists($file)) {
+            file_put_contents($file, '{}');
+        }
+
+        $this->messages = json_decode(file_get_contents($file), true);
+    }
+
+    private function writeMessages(): void
+    {
+        file_put_contents(__DIR__ . '/../../config/chat/' . $this->chatPersistenceId . '.json', json_encode($this->messages));
     }
 
     public function call(
-        $text
+        string $text
     ): string {
-        $rules = 
+        $rules = $this->systemConfig->get('rules', '');
+        assert(is_string($rules));
+
+        $rule = new AIMessage();
+        $rule->role = 'system';
+        $rule->content = $rules;
+
+        $messages = array_merge([$rule], $this->messages);
+
+        $data = [
+            'model' => $this->model,
+            'messages' => $messages,
+            'max_tokens' => 2000,
+            'temperature' => 0.5,
+            'top_p' => 1,
+            'frequency_penalty' => 0,
+            'presence_penalty' => 0,
+            'stop' => ['\r', '\n'],
+        ];
+
+        $jsonData = json_encode($data, JSON_THROW_ON_ERROR);
+
+        $headers = [
+            CURLOPT_URL => $this->url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $jsonData,
+            CURLOPT_HTTPHEADER => [
+                'Content-Type: application/json',
+                'Authorization: Bearer ' . $this->systemConfig->get('openai_api_key', ''),
+            ],
+        ];
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, $headers);
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $response = trim((string) $response);
+
+        try {
+            $decodedResponse = json_decode($response, true, 512, JSON_THROW_ON_ERROR | JSON_INVALID_UTF8_IGNORE);
+        } catch (JsonException) {
+            $decodedResponse = [];
+        }
+
+        return '';
     }
 }
