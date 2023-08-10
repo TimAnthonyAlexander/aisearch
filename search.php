f988087329a4ac21b6bf96216b0247ef2b71e4e7
Tim Anthony Alexander <info@timanthonyalexander.de>
2023-08-10 13:13:05 +0200

    Implement search into search.php

--- search.php
+++ search.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use TimAlexander\Aisearch\Search\Search;
 use TimAlexander\Aisearch\SystemConfig\SystemConfig;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -15,4 +16,17 @@ while (true) {
     if ($term === 'q') {
         break;
     }
+
+    $search = new Search($systemConfig, $term);
+
+    $search->executeSearch();
+
+    $results = $search->getResults();
+
+    if (count($results) === 0) {
+        print "No results found.\n";
+        continue;
+    }
+
+    print json_encode($results, JSON_PRETTY_PRINT);
 }
--- src/Search/Search.php
+++ src/Search/Search.php
@@ -8,8 +8,21 @@ use TimAlexander\Aisearch\SystemConfig\SystemConfig;
 
 class Search
 {
+    private array $results = [];
+
     public function __construct(
-        private SystemConfig $systemConfig
+        private SystemConfig $systemConfig,
+        public readonly string $query,
     ) {
+    }
+
+    public function executeSearch(): void
+    {
+        $this->results = [];
+    }
+
+    public function getResults(): array
+    {
+        return $this->results;
     }
 }
