b08e80f048b51dbbc4e6ddb1d43aae976ba83e32
Tim Anthony Alexander <info@timanthonyalexander.de>
2023-08-10 13:10:04 +0200

    Create SystemConfig

--- src/SystemConfig/SystemConfig.php
+++ src/SystemConfig/SystemConfig.php
@@ -6,7 +6,7 @@ namespace TimAlexander\Aisearch\SystemConfig;
 
 class SystemConfig
 {
-    public array $config;
+    private array $config;
 
     public function __construct() {
         $this->load();
@@ -14,6 +14,42 @@ class SystemConfig
 
     public function load(): void
     {
-        $file = __DIR__ . '/../../config.json';
+        $file = __DIR__ . '/../../config/config.json';
+
+        if (!file_exists($file)) {
+            file_put_contents($file, '{}');
+        }
+
+        $this->config = json_decode(file_get_contents($file), true);
+    }
+
+    public function set(string $key, mixed $value): void
+    {
+        $this->config[$key] = $value;
+        $this->save();
+    }
+
+    public function isset(string $key): bool
+    {
+        return isset($this->config[$key]);
+    }
+
+    public function get(string $key, mixed $defaultValue = null): mixed
+    {
+        if (!$this->isset($key)) {
+            if ($defaultValue !== null) {
+                $this->set($key, $defaultValue);
+                return $defaultValue;
+            }
+
+            return null;
+        }
+
+        return $this->config[$key];
+    }
+
+    public function save(): void
+    {
+        file_put_contents(__DIR__ . '/../../config/config.json', json_encode($this->config));
     }
 }
